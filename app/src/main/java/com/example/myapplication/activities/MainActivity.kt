1c88ba49bc2e9e0ead9d5ec48c4f1f9288297389
Nihad Ildarov <123246218+nihadildarov@users.noreply.github.com>
2023-12-24 17:45:25 +0400

    updated

--- app/src/main/java/com/example/myapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapplication.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,14 +13,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
-
-
+        binding.btmNav.background = null
+        initBottomNav()
     }
 
+
+    private fun initBottomNav(){
+        val navController = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHost = navController.navController
+        binding.btmNav.setupWithNavController(navHost)
+    }
+
+
 }
+
+
