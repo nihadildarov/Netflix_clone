6fc01e3bb04302b97f09806ad398399b5cd5a178
Nihad Ildarov <123246218+nihadildarov@users.noreply.github.com>
2023-12-24 18:37:02 +0400

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,20 +41,19 @@ android {
 }
 
 dependencies {
-    val nav_version = "2.7.5"
+    val nav_version = "2.7.6"
 
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
     implementation("androidx.core:core-ktx:1.12.0")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
 
